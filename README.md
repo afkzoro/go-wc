70381eff52dcb07e8a84c106c70fe7378225e9fa
afkzoro <samueltheophilus019@gmail.com>
2025-01-07 11:06:03 +0100

    chore: project init

---
+++ README.md
@@ -0,0 +1,1 @@
+_Documentation is in the works_
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+
+	"time"
+)
+
+type Counter int
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+
+}
+
+
+func main() {
+
+	showLines := flag.Bool("l", false, "print the newline counts")
+    showBytes := flag.Bool("c", false, "print the byte counts")
+    countWords := flag.Bool("w", false, "Count the number of words in the file")
+    countCharacters := flag.Bool("m", false, "Count the characters in the file")
+    flag.Parse()
+
+
+	args := flag.Args()
+    if len(args) < 1 {
+        fmt.Println("Usage: program [-l|-c] filename")
+        os.Exit(1)
+    }
+
+	filename := args[0]
+
+	if *showBytes {
+        file, err := os.Open(filename)
+        check(err)
+        defer file.Close()
+
+        var c Counter
+        _, err = io.Copy(&c, file)
+        check(err)
+        fmt.Printf("Bytes Count: %v\n", c)
+    }
+
+	if *showLines {
+        buf, err := os.ReadFile(filename)
+        check(err)
+
+        lineCount := bytes.Count(buf, []byte{'\n'})
+        if len(buf) > 0 && !bytes.HasSuffix(buf, []byte{'\n'}) {
+            lineCount++
+        }
+        fmt.Printf("Line Count: %v\n", lineCount)
+    }
+
+    if *countWords {
+
+        startTime := time.Now()
+        file, err := os.Open(filename)
+        check(err)
+
+        reader := bufio.NewReaderSize(file, 16*1024)
+        wordCount := int64(0)
+        inWord := false
+        
+        for {
+            r, _, err := reader.ReadRune()
+            if err != nil {
+                break
+            }
+            
+            if unicode.IsSpace(r) {
+                inWord = false
+            } else if !inWord {
+                wordCount++
+                inWord = true
+            }
+        }
+            elapsedTime := time.Since(startTime).Milliseconds()
+            println("Time elapsed in milliseconds: ", elapsedTime)
+
+            fmt.Printf("Word Count: %v\n", wordCount)
+    }
+
+    if *countCharacters {
+        file, err := os.Open(filename)
+        check(err)
+
+        scanner := bufio.NewScanner(file)
+        scanner.Split(bufio.ScanRunes)
+        charCount := 0
+
+        for scanner.Scan() {
+            charCount++
+        }
+
+        fmt.Printf("Char Count: %v\n", charCount)
+
+    }
+}
+
+// func isWordChar(b byte) bool {
+//     return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
+// }
+
+func (c *Counter) Write (p []byte) (n int, err error) {
+	l := len(p)
+	*c = Counter(int(*c) + l)
+	return l, nil
+}
