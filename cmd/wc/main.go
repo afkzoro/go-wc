982350f54edce65d2afaec0963dd7ff28e9fb000
afkzoro <samueltheophilus019@gmail.com>
2025-02-03 17:05:13 +0100

    feat: handle Fprintf error return

--- cmd/wc/main.go
+++ cmd/wc/main.go
@@ -6,10 +6,9 @@ import (
 	"os"
 
 	"github.com/afkzoro/go-wc/internal/counter"
-	"github.com/afkzoro/go-wc/internal/reader"
 	"github.com/afkzoro/go-wc/internal/printer"
+	"github.com/afkzoro/go-wc/internal/reader"
 )
-
 
 func main() {
 	flags := counter.NewFlags()
@@ -17,13 +16,19 @@ func main() {
 
 	input, filename, err := reader.GetInput(flag.Args())
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		_, err2 := fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		if err2 != nil {
+			return
+		}
 		os.Exit(1)
 	}
 
 	counts, err := counter.Count(input, flags)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		_, err2 := fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		if err2 != nil {
+			return
+		}
 		os.Exit(1)
 	}
 
